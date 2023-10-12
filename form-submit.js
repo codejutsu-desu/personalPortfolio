b6f03e348c1736ff6a640b7fd41a61d0918f7f85
Code-Jutsu <mrimmoys@gmail.com>
2023-10-13 00:22:26 +0600

    cors policy final v2

--- form-submit.js
+++ form-submit.js
@@ -2,31 +2,43 @@ const nodemailer = require("nodemailer");
 
 const sendEmailHandler = async (req, res) => {
   if (req.method === "POST") {
-    const { name, email, message } = req.body;
+    // Set the CORS headers to allow requests from your frontend
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://shimanto-portfolio.vercel.app"
+    ); // Replace with your frontend URL
+    res.setHeader("Access-Control-Allow-Methods", "POST");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-    // Create a Nodemailer transporter
-    const transporter = nodemailer.createTransport({
-      service: "Gmail", // e.g., Gmail
-      auth: {
-        user: "haquemrimmoyka@gmail.com",
-        pass: "doppelganger123@",
-      },
-    });
+    if (req.body) {
+      const { name, email, message } = req.body;
 
-    // Email content
-    const mailOptions = {
-      from: "haquemrimmoyka@gmail.com",
-      to: "mrimmoys@gmail.com", // Your email address where you want to receive messages
-      subject: "New Contact Form Submission",
-      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-    };
+      // Create a Nodemailer transporter
+      const transporter = nodemailer.createTransport({
+        service: "Gmail", // e.g., Gmail
+        auth: {
+          user: "haquemrimmoyka@gmail.com",
+          pass: "doppelganger123@",
+        },
+      });
 
-    try {
-      // Send the email
-      await transporter.sendMail(mailOptions);
-      res.status(200).json({ message: "Email sent successfully" });
-    } catch (error) {
-      res.status(500).json({ message: "Email sending failed" });
+      // Email content
+      const mailOptions = {
+        from: "haquemrimmoyka@gmail.com",
+        to: "mrimmoys@gmail.com", // Your email address where you want to receive messages
+        subject: "New Contact Form Submission",
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+      };
+
+      try {
+        // Send the email
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ message: "Email sent successfully" });
+      } catch (error) {
+        res.status(500).json({ message: "Email sending failed" });
+      }
+    } else {
+      res.status(400).json({ message: "Bad Request: Missing request body" });
     }
   } else {
     res.status(405).end();
